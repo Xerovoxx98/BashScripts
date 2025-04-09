58974a77b81d723a51a0886bdf565ce39da79855
Xerovoxx98 <github@loader.au>
2025-04-09 11:58:22 +1000

    Update lxc-setup.sh

--- lxc-setup.sh
+++ lxc-setup.sh
@@ -8,7 +8,9 @@ apt install git -y
 apt install make -y
 apt install snapd -y
 echo "Installing Neovim"
-snap install nvim --edge --classic
-echo "alias nvim='snap run nvim'" >> ~/.bashrc
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+echo "export PATH='$PATH:/opt/nvim-linux-x86_64/bin'" >> ~/.bashrc
 echo "Installed Neovim"
 
