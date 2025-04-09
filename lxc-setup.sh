257ccb59eda0ca001b6d4ac6c68bb6175c3fe28f
Xerovoxx98 <github@loader.au>
2025-04-09 12:06:24 +1000

    Update lxc-setup.sh

--- lxc-setup.sh
+++ lxc-setup.sh
@@ -14,4 +14,3 @@ echo "Installed Neovim"
 echo "Installing Sexy Bash"
 (cd /tmp && ([[ -d sexy-bash-prompt ]] || git clone --depth 1 --config core.autocrlf=false https://github.com/twolfson/sexy-bash-prompt) && cd sexy-bash-prompt && make install) && source ~/.bashrc
 echo "Installed Sexy Bash"
-source ~/.bashrc
