b4c918e4ef1cb4edd1baf7cbf1e98a464d837d7a
Xerovoxx98 <github@loader.au>
2025-04-09 17:14:56 +1000

    Update lxc-setup.sh

--- lxc-setup.sh
+++ lxc-setup.sh
@@ -10,6 +10,7 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 echo "export PATH='$PATH:/opt/nvim-linux-x86_64/bin'" >> ~/.bashrc
+sudo rm nvim-linux-x86_64.tar.gz
 echo "Installed Neovim"
 echo "Installing Sexy Bash"
 (cd /tmp && ([[ -d sexy-bash-prompt ]] || git clone --depth 1 --config core.autocrlf=false https://github.com/twolfson/sexy-bash-prompt) && cd sexy-bash-prompt && make install) && source ~/.bashrc
