67246080f85a548b19e4cf6bff29bf5dd9ff697e
Xerovoxx98 <github@loader.au>
2025-04-12 16:55:54 +1000

    Update lxc-setup.sh

--- lxc-setup.sh
+++ lxc-setup.sh
@@ -1,6 +1,6 @@
 echo "Installing APT-Proxies"
 echo 'Acquire::http::Proxy-Auto-Detect "/usr/local/bin/apt-proxy-detect.sh";' > /etc/apt/apt.conf.d/00aptproxy
-echo -e '#!/bin/bash\nif nc -w1 -z "10.0.1.5" 3142; then\n  echo -n "http://10.0.1.5:3142"\nelif nc -w1 -z "10.0.1.6" 3142; then\n  echo -n "http://10.0.1.6:3142"\nelse\n  echo -n "DIRECT"\nfi' > /usr/local/bin/apt-proxy-detect.sh
+echo -e '#!/bin/bash\nif nc -w1 -z "10.0.1.3" 3142; then\n  echo -n "http://10.0.1.3:3142"\n else\n  echo -n "DIRECT"\nfi' > /usr/local/bin/apt-proxy-detect.sh
 chmod +x /usr/local/bin/apt-proxy-detect.sh
 echo "Installed APT Proxies"
 
