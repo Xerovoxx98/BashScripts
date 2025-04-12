cbd3d2a007e31f514bd27068c7faf96f63ba49af
Xerovoxx98 <github@loader.au>
2025-04-12 11:56:48 +1000

    Update

--- cockpit-setup.sh
+++ cockpit-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 apt update && apt upgrade
-apt install gpg git lsb-core
+apt install gpg git
 curl -sSL https://repo.45drives.com/setup | bash
 apt update && apt upgrade -y
-apt install cockit-file-sharing -y
+apt install cockpit-file-sharing -y
 apt install cockpit-identities -y
 apt install cockpit-navigator -y
